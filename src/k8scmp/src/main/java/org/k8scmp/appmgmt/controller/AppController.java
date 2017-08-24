ace80b9c8231fc29979662501dec181ce9d3a58f
me <me@yan-pc>
2017-08-24 13:27:08 +0800

    cccc

--- src/k8scmp/src/main/java/org/k8scmp/appmgmt/controller/AppController.java
+++ src/k8scmp/src/main/java/org/k8scmp/appmgmt/controller/AppController.java
@@ -2,6 +2,8 @@ package org.k8scmp.appmgmt.controller;
 
 import org.k8scmp.appmgmt.domain.AppInfo;
 import org.k8scmp.appmgmt.service.AppService;
+import org.k8scmp.basemodel.HttpResponseTemp;
+import org.k8scmp.basemodel.ResultStat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,13 +27,13 @@ public class AppController {
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public HttpResponseTemp<?> deleteApp(@PathVariable int id) throws IOException {
+    public HttpResponseTemp<?> deleteApp(@PathVariable String id) throws IOException {
         return appService.deleteApp(id);
     }
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public HttpResponseTemp<?> modifyApp(@PathVariable int id,
+    public HttpResponseTemp<?> modifyApp(@PathVariable String id,
                                                    @RequestBody AppInfo appInfo) throws Exception {
         return appService.modifyApp(id, appInfo);
     }
