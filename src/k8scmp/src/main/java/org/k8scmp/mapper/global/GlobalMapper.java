d7a3d8f920b8032d6a8f66267be0d8f55097d456
jbyb <by745640582@163.com>
2017-08-25 17:00:01 +0800

    graph

--- src/k8scmp/src/main/java/org/k8scmp/mapper/global/GlobalMapper.java
+++ src/k8scmp/src/main/java/org/k8scmp/mapper/global/GlobalMapper.java
@@ -11,32 +11,32 @@ import java.util.List;
  */
 @Mapper
 public interface GlobalMapper {
-    @Insert("INSERT INTO global (type, value, createTime, lastUpdate, description) values (" +
+    @Insert("INSERT INTO globalconfig (type, value, createTime, lastUpdate, description) values (" +
             "#{type}, #{value}, #{createTime}, #{lastUpdate}, #{description})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     int addGlobalInfo(GlobalInfo globalInfo);
 
-    @Select("SELECT * FROM global WHERE type=#{type}")
+    @Select("SELECT * FROM globalconfig WHERE type=#{type}")
     GlobalInfo getGlobalInfoByType(@Param("type") GlobalType globalType);
 
-    @Select("SELECT * FROM global WHERE type=#{type} AND id=#{id}")
+    @Select("SELECT * FROM globalconfig WHERE type=#{type} AND id=#{id}")
     GlobalInfo getGlobalInfoByTypeAndId(@Param("type") GlobalType globalType, @Param("id") int id);
 
-    @Select("SELECT * FROM global WHERE type=#{type}")
+    @Select("SELECT * FROM globalconfig WHERE type=#{type}")
     List<GlobalInfo> listGlobalInfoByType(@Param("type") GlobalType globalType);
 
-    @Select("SELECT * FROM global WHERE id=#{id}")
+    @Select("SELECT * FROM globalconfig WHERE id=#{id}")
     GlobalInfo getGlobalInfoById(@Param("id") int id);
 
-    @Delete("DELETE FROM global WHERE type=#{type}")
+    @Delete("DELETE FROM globalconfig WHERE type=#{type}")
     int deleteGlobalInfoByType(@Param("type") GlobalType globalType);
 
-    @Delete("DELETE FROM global WHERE id=#{id}")
+    @Delete("DELETE FROM globalconfig WHERE id=#{id}")
     int deleteGlobalInfoById(@Param("id") int id);
 
-    @Update("UPDATE global SET value=#{value}, lastUpdate=#{lastUpdate}, description=#{description} WHERE type=#{type} AND id=#{id}")
+    @Update("UPDATE globalconfig SET value=#{value}, lastUpdate=#{lastUpdate}, description=#{description} WHERE type=#{type} AND id=#{id}")
     int updateGlobalInfoById(GlobalInfo globalInfo);
 
-    @Update("UPDATE global SET value=#{value}, lastUpdate=#{lastUpdate} WHERE type=#{type}")
+    @Update("UPDATE globalconfig SET value=#{value}, lastUpdate=#{lastUpdate} WHERE type=#{type}")
     int updateGlobalInfoByType(GlobalInfo globalInfo);
 }
