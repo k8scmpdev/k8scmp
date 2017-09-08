7a2b8896479c027c8b01e28dd0ac534d907315bc
vivianzttgithub <1642651996@qq.com>
2017-09-08 10:08:30 +0800

    tight style

--- src/k8scmp/src/main/resources/static/js/unicorn.tables.js
+++ src/k8scmp/src/main/resources/static/js/unicorn.tables.js
@@ -5,9 +5,29 @@
 $(document).ready(function(){
 	
 	$('.data-table').dataTable({
+		"bProcessing" : true,
 		"bFilter":false,
 		"bJQueryUI": true,
 		"sPaginationType": "full_numbers",
+		"bScrollCollapse" : true,
+		"bAutoWidth": false,
+		"sScrollY": ($(window).height() - 320),
+		"oLanguage": {
+						"sProcessing": "正在加载中......",
+                        "sLengthMenu": "每页显示 _MENU_ 条记录",
+                        "sZeroRecords": "正在加载中......",
+                        "sEmptyTable": "查询无数据！",
+                        "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_ 条记录",
+                        "sInfoEmpty": "显示0到0条记录",
+                        "sInfoFiltered": "数据表中共为 _MAX_ 条记录",
+                        "sSearch": "当前数据搜索",
+                        "oPaginate": {
+                        	"sFirst": "首页",
+                            "sPrevious": "上一页",
+                            "sNext": "下一页",
+                            "sLast": "末页"
+                        }
+                   	},
 		"sDom": '<""l>t<"F"fp>'
 	});
 	
