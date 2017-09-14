decd2b759129adee54f363f12e96c0328ddb86b8
yanhaili <31228325+yanhaili@users.noreply.github.com>
2017-09-14 15:01:29 +0800

    aa

--- src/k8scmp/src/main/java/org/k8scmp/mapper/operation/OperationMapper.java
+++ src/k8scmp/src/main/java/org/k8scmp/mapper/operation/OperationMapper.java
@@ -13,15 +13,15 @@ import java.util.List;
 @Mapper
 public interface OperationMapper {
 	String BASIC_COLUMN =  " id, resourceId, resourceType, operation, userId, userName, status, message, operateTime";
-    @Insert("insert into operation_history (id, resourceId, resourceType, operation, userId, userName, status, message, operateTime)" +
+    @Insert("insert into operationLog (id, resourceId, resourceType, operation, userId, userName, status, message, operateTime)" +
             " values (#{id}, #{resourceId}, #{resourceType}, #{operation}, #{userId}, #{userName}, #{status}, #{message}, #{operateTime})")
     int insertRecord(OperationRecord record);
 
-    @Select("select"+BASIC_COLUMN+" from operation_history where id = #{id}")
+    @Select("select"+BASIC_COLUMN+" from operationLog where id = #{id}")
     OperationRecord getById(int id);
 
     //@Todo resourceType needed to be removed
-    @Select("SELECT"+BASIC_COLUMN+" FROM operation_history WHERE userId = #{userId} AND operateTime >= #{operateTime}"
+    @Select("SELECT"+BASIC_COLUMN+" FROM operationLog WHERE userId = #{userId} AND operateTime >= #{operateTime}"
             + " AND resourceType in ('PROJECT','PROJECT_COLLECTION'"
             + ",'DEPLOY','DEPLOY_COLLECTION'"
             + ",'CONFIGURATION','CONFIGURATION_COLLECTION'"
