558e9e7e1ff4ac34735ed5109bbea06742a28718
wjs7740 <wjs7740@163.com>
2017-09-15 14:35:59 +0800

    overview

--- src/k8scmp/src/main/java/org/k8scmp/overview/controller/OverviewController.java
+++ src/k8scmp/src/main/java/org/k8scmp/overview/controller/OverviewController.java
@@ -5,13 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.k8scmp.login.domain.User;
 import org.k8scmp.operation.OperationLog;
 import org.k8scmp.operation.OperationRecord;
@@ -43,6 +48,26 @@ public class OverviewController {
         User user = AuthUtil.getUser();
      	model.addAttribute("user", user);
         return "overview/index";
+    }
+    
+    @RequestMapping(value="/overview/appinfo", method=RequestMethod.GET)
+    public JSONObject showAppInfo() {
+     	
+     	JSONObject appinfo = new JSONObject();
+     	try {
+//     		String json = "{'type':'pie','name':'应用'}";
+     		appinfo.put("type", "pie");
+     		appinfo.put("name", "应用");
+     		Map <String, Integer> data = new HashMap <String, Integer>();
+     		data.put("运行中: 3",   3);
+     		data.put("已停止: 2",   2);
+     		data.put("操作中: 1",   1);
+			appinfo.put("data", data);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return appinfo;
     }
    
    
