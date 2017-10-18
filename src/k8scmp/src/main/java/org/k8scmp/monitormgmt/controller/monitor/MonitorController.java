61f3190ccbdbae00248332b82d28f228e9181355
jbyb <by745640582@163.com>
2017-10-18 09:27:26 +0800

    moditorController

--- src/k8scmp/src/main/java/org/k8scmp/monitormgmt/controller/monitor/MonitorController.java
+++ src/k8scmp/src/main/java/org/k8scmp/monitormgmt/controller/monitor/MonitorController.java
@@ -1,8 +1,10 @@
 package org.k8scmp.monitormgmt.controller.monitor;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.k8scmp.basemodel.HttpResponseTemp;
+import org.k8scmp.basemodel.ResultStat;
 import org.k8scmp.monitormgmt.domain.monitor.InstenceInfoBack;
 import org.k8scmp.monitormgmt.domain.monitor.NodeInfoBack;
 import org.k8scmp.monitormgmt.service.monitor.MonitorService;
@@ -30,11 +32,17 @@ public class MonitorController {
 	
 	@RequestMapping(value = "/hostlist",method = RequestMethod.GET)
     public String getMonitorHostlist(Model model) throws Exception {
-		List<NodeInfoBack> nodeInfoList = monitorService.getNodeMonitorData();
+		List<NodeInfoBack> nodeInfoList = monitorService.getNodeMonitorData(null);
 		model.addAttribute("nodeInfoList", nodeInfoList);
 		model.addAttribute("total", nodeInfoList.size());
         return "monitor/monitor-hostlist";
-		
+    }
+	
+	@ResponseBody
+	@RequestMapping(value = "/hostsearch", method = RequestMethod.POST)
+    public HttpResponseTemp<?> getMonitorHostsearch(@RequestParam("hostName") String hostName) throws Exception {
+		List<NodeInfoBack> nodeInfoList = monitorService.getNodeMonitorData(hostName);
+		return ResultStat.OK.wrap(nodeInfoList);
     }
 	
 	@RequestMapping(value = "/instencelist",method = RequestMethod.GET)
@@ -45,26 +53,37 @@ public class MonitorController {
         return "monitor/monitor-instencelist";
     }
 	
-	@RequestMapping(value = "/host",method = RequestMethod.GET)
+	@ResponseBody
+	@RequestMapping(value = "/instenceSearch", method = RequestMethod.POST)
+    public HttpResponseTemp<?> getMonitorInstenceSearch(@RequestParam("serviceName") String serviceName) throws Exception {
+		List<InstenceInfoBack> instenceList = monitorService.getInstenceMonitorData(serviceName);
+        return ResultStat.OK.wrap(instenceList);
+    }
+	
+	@RequestMapping(value = "/host", method = RequestMethod.GET)
     public String getMonitorHost(Model model) throws Exception {
 		//默认监控当前时间1小时内的值  单个主机的指标展示
 		String type = "node";
-		long end = System.currentTimeMillis();
-		long start = end - 1000*60*60;
-		String dataSpec = "average";
-		model.addAttribute("nodeData", getMonitorData(type,start,end,dataSpec));
+		Calendar current = Calendar.getInstance();
+        long endTime = current.getTimeInMillis();
+        current.set(Calendar.MINUTE, current.get(Calendar.MINUTE) - 60*60);
+        long startTime = current.getTimeInMillis();
+		String dataSpec = "AVERAGE";
+		model.addAttribute("nodeData", getMonitorData(type,startTime,endTime,dataSpec));
 		
         return "monitor/monitor-host";
     }
 	
-	@RequestMapping(value = "/instence")
+	@RequestMapping(value = "/instence", method = RequestMethod.GET)
     public String getMonitorInstence(Model model) throws Exception {
 		//默认监控当前时间1小时内的值 单个实例的指标展示
 		String type = "pod";
-		long end = System.currentTimeMillis();
-		long start = end - 1000*60*60;
-		String dataSpec = "average";
-		model.addAttribute("instenceData", getMonitorData(type,start,end,dataSpec));
+		Calendar current = Calendar.getInstance();
+        long endTime = current.getTimeInMillis();
+        current.set(Calendar.MINUTE, current.get(Calendar.MINUTE) - 60*60);
+        long startTime = current.getTimeInMillis();
+		String dataSpec = "AVERAGE";
+		model.addAttribute("instenceData", getMonitorData(type,startTime,endTime,dataSpec));
 				
         return "monitor/monitor-instence";
     }
