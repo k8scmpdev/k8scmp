16521f49f0fa28a8641527df9feae6e7439f1e0d
vivianzttgithub <1642651996@qq.com>
2017-10-24 14:57:27 +0800

    ewqe
    qdqdwq

--- src/k8scmp/src/main/resources/static/js/app/service-new.js
+++ src/k8scmp/src/main/resources/static/js/app/service-new.js
@@ -480,7 +480,13 @@ $("#btnnew3pre").bind("click", function(event) {
 	var image = $("#imageConfig").val();
 	
 	var tag = $("#tagConfig").val();
-	var commands = $("#startCommands").val().split(",");
+	var commands = null;
+	if($("#startCommands").val() == ""){
+		commands = null;
+	}else{
+		commands = $("#startCommands").val().split(",");
+	}
+	
 	var envs = getEnvsToArray($("#envvar").val());
 	var cpu = $("#cpus").val();
 	var mem = $("#mems").val();
